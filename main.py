be9caae95a894aa506729c7a4b0d6e01d0e37106
Louistam888 <louistam888@gmail.com>
2023-04-14 11:15:56 -0400

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import random 
 
-def deal_card():
+def dealCard():
+  # returns a random card from the deck
   cards = [11, 2, 3, 4, 5, 6, 7, 8,9 , 19, 10, 10, 10, 10]
   card = random.choice(cards)
   return card
 
+userCards = []
+computerCards = []
+
+for _ in range(2):
+  userCards.append(dealCard())
+  computerCards.append(dealCard())
